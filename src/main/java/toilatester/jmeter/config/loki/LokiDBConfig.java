91a279da02707be411cca91da1bd64327c45b65f
minhhoang <minhhoang@kms-technology.com>
2020-12-06 16:45:57 +0700

    Init Loki

---
+++ src/main/java/toilatester/jmeter/config/loki/LokiDBConfig.java
@@ -0,0 +1,9 @@
+package toilatester.jmeter.config.loki;
+
+import org.apache.jmeter.visualizers.backend.BackendListenerContext;
+
+public class LokiDBConfig {
+	public LokiDBConfig(BackendListenerContext context) {
+		
+	}
+}
