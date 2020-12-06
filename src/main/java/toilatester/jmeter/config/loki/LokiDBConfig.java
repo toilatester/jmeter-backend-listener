d85e39e09d6e7f8f1363eb9ce548b67b54749a25
minhhoang <minhhoang@kms-technology.com>
2020-12-06 17:19:47 +0700

    Init

--- src/main/java/toilatester/jmeter/config/loki/LokiDBConfig.java
+++ src/main/java/toilatester/jmeter/config/loki/LokiDBConfig.java
@@ -3,6 +3,11 @@ package toilatester.jmeter.config.loki;
 import org.apache.jmeter.visualizers.backend.BackendListenerContext;
 
 public class LokiDBConfig {
+	public static final String DEFAULT_PROTOCOL = "http";
+	public static final String DEFAULT_HOST = "localhost";
+	public static final int DEFAULT_PORT = 3100;
+	
+	
 	public LokiDBConfig(BackendListenerContext context) {
 		
 	}
