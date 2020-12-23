86e9e7faadf7f7780c6bbdd478607087acc6281c
hnminh <hnminh@outlook.com>
2020-12-24 01:23:04 +0700

    feat: update unit test

--- src/main/java/toilatester/jmeter/report/exception/ReportException.java
+++ src/main/java/toilatester/jmeter/report/exception/ReportException.java
@@ -17,13 +17,4 @@ public class ReportException extends RuntimeException{
 	public ReportException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
-	public ReportException(Throwable cause) {
-		super(cause);
-	}
-
-	public ReportException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
 }
